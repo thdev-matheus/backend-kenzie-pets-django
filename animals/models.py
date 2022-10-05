4dc4c325ad5fc781a585178eb74f1d756f1e5c0d
Theus <matheusth.dev@gmail.com>
2022-10-05 00:17:48 -0300

    feat: Animal model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class AnimalSex(models.TextChoices):
+    MACHO = "Macho"
+    FEMEA = "Fêmea"
+    NAO_INFORMADO = "Não Informado"
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=15, choices=AnimalSex.choices, default=AnimalSex.NAO_INFORMADO
+    )
